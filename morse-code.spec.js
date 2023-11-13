bdd68ec0226f6fb5b794ecf34a43fda08e51a0e1
joaosantosmedeiros <jopesame@gmail.com>
2023-11-13 16:03:12 -0300

    remove test files

--- morse-code.spec.js
+++
@@ -1,8 +0,0 @@
-import { expect, test } from "bun:test";
-const { decodeMorse } = require('./morse-code')
-
-test("decodeMorse", () => {
-  expect(decodeMorse('.... . -.--   .--- ..- -.. .')).toBe('HEY JUDE');
-  expect(decodeMorse('   .... . -.--   ')).toBe('HEY');
-  expect(decodeMorse('      ...---... -.-.--   - .... .   --.- ..- .. -.-. -.-   -... .-. --- .-- -.   ..-. --- -..-   .--- ..- -- .--. ...   --- ...- . .-.   - .... .   .-.. .- --.. -.--   -.. --- --. .-.-.-  ')).toBe('SOS! THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.');
-});
