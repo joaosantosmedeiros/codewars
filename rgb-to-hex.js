c1d6ed2c693383c4014ea868c0b6d77fb0ffdd81
darthjotinha <jopesame@gmail.com>
2023-12-04 10:43:11 -0300

    move javascript files to separate folder

--- rgb-to-hex.js
+++
@@ -1,9 +0,0 @@
-// https://www.codewars.com/kata/513e08acc600c94f01000001/train/javascript
-function rgb(r, g, b) {
-  return [r, g, b].map((value, index, arr) => {
-      if (arr[index] > 255) arr[index] = 255;
-      if (arr[index] < 0) arr[index] = 0;
-
-      return Math.floor(arr[index] / 16).toString(16) + (arr[index] % 16).toString(16);
-    }).join("").toUpperCase();
-}
