a6c2d5eab3c2c8a368dcef8efe2b8ecb5496aac5
darthjotinha <jopesame@gmail.com>
2024-02-13 15:30:59 -0300

    string to number kata

---
+++ java/8 kyu/StringToNumber.java
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/544675c6f971f7399a000e79/train/java
+public class StringToNumber {
+  public static int stringToNumber(String str) {
+    return Integer.valueOf(str);
+  }
+}
