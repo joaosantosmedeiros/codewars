f771aaee4b39a9f8173ae578a4d1d02174a26a79
joaosantosmedeiros <jopesame@gmail.com>
2024-03-25 20:08:53 -0300

    more than zero kata

---
+++ java/7 kyu/MoreThanZero.java
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/55a710b462afc49a540000b9/train/java
+public class MoreThanZero {
+  public static String corrections(int x) {
+    return x > 0 ? x + " is more than zero." : x + " is equal to or less than zero.";
+  }
+}
