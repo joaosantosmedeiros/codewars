4c610611f281b4ae98e22ffdff9b68d4e548a9b1
joaosantosmedeiros <jopesame@gmail.com>
2024-06-17 20:29:48 -0300

    number of decimal digits kata

---
+++ java/7 kyu/DecTools.java
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/58fa273ca6d84c158e000052
+public class DecTools {
+  public static int Digits(long n) {
+    return String.valueOf(n).length();
+  }
+}
--- java/7 kyu/FlattenAndSort.java
+++ java/7 kyu/FlattenAndSort.java
@@ -1,4 +1,3 @@
-
 // https://www.codewars.com/kata/57ee99a16c8df7b02d00045f
 import java.util.Arrays;
 import java.util.stream.IntStream;
