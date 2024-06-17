4ce9eee1246246cb9668135ac27718a9e752f513
joaosantosmedeiros <jopesame@gmail.com>
2024-06-17 11:08:02 -0300

    get the middle character kata

---
+++ java/7 kyu/kata.java
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/56747fd5cb988479af000028/
+
+class MiddleCharacter {
+  public static String getMiddle(String word) {
+    int length = word.length();
+    return (length % 2 == 0 ? word.charAt(length / 2 - 1) + "" : "") + word.charAt(length / 2) + "";
+  }
+}
