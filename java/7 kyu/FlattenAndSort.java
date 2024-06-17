0e517c43a09f3a3d77aeb8994aad55c66b90364b
joaosantosmedeiros <jopesame@gmail.com>
2024-06-17 20:27:26 -0300

    flatten and sort array kata

---
+++ java/7 kyu/FlattenAndSort.java
@@ -0,0 +1,11 @@
+
+// https://www.codewars.com/kata/57ee99a16c8df7b02d00045f
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class FlattenAndSort {
+
+	public static int[] flattenAndSort(int[][] array) {
+		return Arrays.stream(array).flatMapToInt(subArr -> IntStream.of(subArr)).sorted().toArray();
+	}
+}
