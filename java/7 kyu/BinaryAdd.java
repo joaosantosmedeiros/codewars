d46a1e846f48bf9b66182793bb50416cb45b645b
joaosantosmedeiros <jopesame@gmail.com>
2024-06-17 12:11:49 -0300

    binary addition kata

---
+++ java/7 kyu/BinaryAdd.java
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/551f37452ff852b7bd000139
+public class BinaryAdd{
+  
+  public static String binaryAddition(int a, int b){
+    return Integer.toBinaryString(a + b);
+  }
+}
