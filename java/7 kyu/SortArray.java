e52d31635944542a94836149cb6b95a75244970f
joaosantosmedeiros <jopesame@gmail.com>
2024-06-18 11:14:16 -0300

    sort arrays kata

---
+++ java/7 kyu/SortArray.java
@@ -0,0 +1,35 @@
+// https://www.codewars.com/kata/51f41b98e8f176e70d0002a8/
+import java.util.Arrays;
+
+public class SortArray {
+
+  public static int[] sortArray(int[] names) {
+    Arrays.sort(names);
+    return names;
+  }
+
+  public static Integer[] sortArray(Integer[] names) {
+    Arrays.sort(names);
+    return names;
+  }
+
+  public static long[] sortArray(long[] names) {
+    Arrays.sort(names);
+    return names;
+  }
+
+  public static double[] sortArray(double[] names) {
+    Arrays.sort(names);
+    return names;
+  }
+
+  public static float[] sortArray(float[] names) {
+    Arrays.sort(names);
+    return names;
+  }
+
+  public static String[] sortArray(String[] names) {
+    Arrays.sort(names);
+    return names;
+  }
+}
