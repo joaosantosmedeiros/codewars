d622b20c4a23e1f01e570f1a29f792c205528f0b
joaosantosmedeiros <jopesame@gmail.com>
2024-07-19 09:50:48 -0300

    arguments kata

---
+++ java/7 kyu/Arguments.java
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/5c44b0b200ce187106452139
+public class Arguments {
+  public static int countArgs(Object... args) {
+    // your code here
+    return args.length;
+  }
+}
