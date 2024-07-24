f2ce749ea936363d69e5c9342d15e282f5587f3b
joaosantosmedeiros <jopesame@gmail.com>
2024-07-24 15:50:00 -0300

    is it a vowel kata

---
+++ java/7 kyu/IsItAVowel.java
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/567bed99ee3451292c000025
+public class IsItAVowel {
+
+  public static boolean isVowel(String s) {
+    return s.toLowerCase().matches("[aeiou]");
+  }
+}
