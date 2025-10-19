d03bfbb0283ecef6eef3b90baba9bef0a55a9721
João Pedro <93298952+joaosantosmedeiros@users.noreply.github.com>
2025-10-19 12:43:02 -0300

    reaf

--- c/8kyu/was_package_received_yesterday.c
+++ c/8kyu/was_package_received_yesterday.c
@@ -2,5 +2,5 @@
 #include<stdbool.h>
 
 bool was_package_received_yesterday(int tz_from, int tz_to, int start, int duration) {                     
-  return (tz_to - tz_from) + start + duration < 0 ? true : false;                                                               
+  return (tz_to - tz_from) + start + duration < 0;
 }                                         
