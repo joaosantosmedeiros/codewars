44cbc7867ffa2448334bfec81963a8ec3ac5846e
João Pedro <93298952+joaosantosmedeiros@users.noreply.github.com>
2025-10-24 15:04:25 -0300

    Add basic_op function for basic arithmetic operations

---
+++ c/8kyu/calculator.c
@@ -0,0 +1,12 @@
+//https://www.codewars.com/kata/57356c55867b9b7a60000bd7
+int basic_op(char op, int value1, int value2) {
+  switch(op) {
+      case '+': 
+        return value1 + value2;
+      case '-':
+        return value1 - value2;
+      case '*':
+        return value1 * value2;
+  }
+  return value1 / value2;
+}
